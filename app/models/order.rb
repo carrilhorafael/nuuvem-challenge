fa16a93bb14ff4595d98a98695e61590a8c3aa8d
Carrilho <rafael.carrilho@injunior.com.br>
2021-07-15 14:43:43 -0300

    Table with Total price

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,15 @@ class Order < ApplicationRecord
   validates :client, :product, :quantity, presence: true
   validates :quantity, numericality: {greater_than: 0}
 
-  def total_price
+  def order_price
     self.quantity * self.product.price
+  end
+  def self.total_price
+    sum = 0
+    all_prices = Order.all.map{|order| order.order_price}
+    all_prices.each do |price|
+      sum += price
+    end
+    sum
   end
 end
