9d34acb30c58ee3a5b1adea63023dfce6ef5018f
Carrilho <rafael.carrilho@injunior.com.br>
2021-07-17 10:23:56 -0300

    correções simples nuuvem challenge

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,18 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   it 'is valid with all params' do
-    merchant = Merchant.create!(name: "Donuts House", address: "351 Park Avenue, NY") 
+    merchant = Merchant.create(name: "Donuts House", address: "351 Park Avenue, NY") 
     expect(merchant).to be_valid 
   end
-
-  it 'is invalid without name' do
-    merchant = Merchant.create(name: nil, address: "351 Park Avenue, NY") 
-    expect(merchant).to_not be_valid
-  end
-  
-  it 'is invalid without address' do
-    merchant = Merchant.create(name: "Donuts House", address: nil)
-    expect(merchant).to_not be_valid
+  context 'validations test' do
+    it 'when without name' do
+      merchant = Merchant.create(name: nil, address: "351 Park Avenue, NY") 
+      merchant.valid?
+      expect(merchant.errors[:name]).to include("can't be blank")
+    end
+    
+    it 'when without address' do
+      merchant = Merchant.create(name: "Donuts House", address: nil)
+      merchant.valid?
+      expect(merchant.errors[:address]).to include("can't be blank")
+    end
   end
 
 end
